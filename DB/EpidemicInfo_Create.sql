a2f1cdc4f84b230554d70dc18679e974ff997900
_mikuappend <mikuappendg@gmail.com>
2020-05-17 00:03:23 +0800

    Created SQL database.

---
+++ DB/EpidemicInfo_Create.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE "epidemicinfo"
+(
+  "CName" text NOT NULL, -- City English name.
+  CONSTRAINT "CNamePri" PRIMARY KEY ("CName"),
+  CONSTRAINT "CNameForeign" FOREIGN KEY ("CName")
+      REFERENCES "regioninfo" ("CName") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=TRUE
+);
+ALTER TABLE "epidemicinfo"
+  OWNER TO postgres;
+GRANT SELECT, REFERENCES ON TABLE public.epidemicinfo TO public;
+COMMENT ON TABLE "epidemicinfo"
+  IS 'Daily epidemic info.';
+COMMENT ON COLUMN "epidemicinfo"."CName" IS 'City English name.';
