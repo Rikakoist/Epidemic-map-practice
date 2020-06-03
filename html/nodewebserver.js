19eb20e978e1a6d29e6f1c9931fdb8e62a407f23
_mikuappend <mikuappendg@gmail.com>
2020-06-03 23:39:25 +0800

    merged pgpg and http

--- html/nodewebserver.js
+++ html/nodewebserver.js
@@ -1,6 +1,32 @@
 var http = require('http');
+var pg = require('pg');
 
 http.createServer(function(request, response) {
+
+    var conString = "postgres://username:password@loc/db";
+
+    var client = new pg.Client(conString);
+
+    SQLString = 'SELECT * FROM "public".epidemicinfo WHERE "public".epidemicinfo."CName" = \'Aichi\'';
+
+    client.connect(function(error, results) {
+        if (error) {
+            console.log('clientConnectionReady Error:' + error.message);
+            client.end();
+            return;
+        }
+        console.log('connection success...\n');
+        client.query(SQLString, function(error, results) {
+            console.log(error);
+            return results;
+        })
+        return results;
+    });
+
+
+    //ClientConnectionReady Error:getaddrinfo ENOTFOUND public
+
+
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write("response content.");
     response.end('Hello World\n');
