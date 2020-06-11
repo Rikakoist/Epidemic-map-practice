28f0bb060a4abcf38501c4305360c03ffba09ec9
_mikuappend <mikuappendg@gmail.com>
2020-06-11 10:54:58 +0800

    express

--- html/exp.js
+++ html/exp.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function(req, res) {
-    res.send('hello world');
+    console.log(req.cookies);
+    res.send('hello');
+})
+
+app.get('/covid19data', function(req, res) {
+    consile.log(req);
 })
 
 var server = app.listen(8081, function() {
